637c71a87a4100e45def0b3d8eb86d383defdf91
Rilega <leandroriosga@gmail.com>
2022-06-24 09:40:31 -0300

    finalizo parte 1 del tp Mercado Liebre

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3030;
+
+app.use(express.static(path.join(__dirname, "public")))
+app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "views", "home.html")))
+
+app.listen(port, () => console.log(`Se levanto el servidor con exito en el http://localhost:${port}`))
