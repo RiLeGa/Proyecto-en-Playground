c3e0e0ec30369cacc618875b2d2fa2d678836e88
Rilega <leandroriosga@gmail.com>
2022-06-30 04:18:03 -0300

    Modifico app.js y agrego archivos css

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express()
+const port = 3010
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
+
+
+app.listen(port, () => console.log(`El servidor se levanto con exito en http://localhost:${port}`))
